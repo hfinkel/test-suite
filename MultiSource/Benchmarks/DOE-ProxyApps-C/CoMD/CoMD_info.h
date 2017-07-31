3d0eac91080e0bd2f2106fa7787aca7c2d222f7b
akhilpatlolla <patlolla.akhil@gmail.com>
2017-07-31 21:22:28 +0000

    Added CoMD Proxy App

---
+++ MultiSource/Benchmarks/DOE-ProxyApps-C/CoMD/CoMD_info.h
@@ -0,0 +1,15 @@
+#ifndef CoMD_info_hpp
+#define CoMD_info_hpp
+
+#define CoMD_VARIANT "CoMD-serial"
+#define CoMD_HOSTNAME "jlselogin1"
+#define CoMD_KERNEL_NAME "'Linux'"
+#define CoMD_KERNEL_RELEASE "'3.10.0-514.6.1.el7.x86_64'"
+#define CoMD_PROCESSOR "'x86_64'"
+
+#define CoMD_COMPILER "'/bin/gcc'"
+#define CoMD_COMPILER_VERSION "'gcc (GCC) 4.8.5 20150623 (Red Hat 4.8.5-11)'"
+#define CoMD_CFLAGS "'-std=c99 -DDOUBLE -g -O5  '"
+#define CoMD_LDFLAGS "'-lm '"
+
+#endif
